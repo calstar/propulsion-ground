e3232c7bbce46b0adcf5490664df2db2f82027de
Rajiv Govindjee <rgovindjee@berkeley.edu>
2019-04-18 15:30:20 -0700

    bugfixes for servo testing code

--- servo_test/servo_test.ino
+++ servo_test/servo_test.ino
@@ -11,27 +11,52 @@ uint8_t servoAngle = 0;
 
 void setup() {
   //map servo objects to pins on Arduino
-  myservo_1.attach(6);
-  myservo_2.attach(9);
-  myservo_3.attach(11);
+  //attach(pin, min pulse width in usec, max pulse width in usec)
+  myservo_1.attach(6,870,2320);
+  myservo_2.attach(9,870,2320);
+  myservo_3.attach(11,870,2320);
 
   //start reading data over serial in
   Serial.begin(9600); 
 }
 
 void loop() {
-
+//  myservo_1.write(80);
+//  delay(500);
+//  myservo_2.write(80);
+//  delay(500);
+//  myservo_3.write(80);
+//  delay(500);
+//  myservo_1.write(170);
+//  delay(500);
+//  myservo_2.write(170);
+//  delay(500);
+//  myservo_3.write(170);
+//  delay(500);
   if (Serial.available() > 0) {
           // read the incoming two numbers:
           servoSelect = Serial.parseInt();
+          bool badData = 0;
+          //make sure we selected a valid servo
+          if (servoSelect != 1 && servoSelect != 2 && servoSelect != 3) {
+            badData = 1;
+          }
           servoAngle = Serial.parseInt();
-          // say what you got:
-          Serial.println("I received: ");
-          Serial.print("Servo : ");
-          Serial.println(servoSelect, DEC);
-          Serial.print("Angle: ");
-          Serial.println(servoAngle, DEC);
-          Serial.println("----");
+          if (servoAngle > 255 || servoAngle < 0) {
+            badData = 1;
+          }
+          Serial.flush();
+          if (!badData) {
+            // say what you got:
+            Serial.println("I received: ");
+            Serial.print("Servo : ");
+            Serial.println(servoSelect, DEC);
+            Serial.print("Angle: ");
+            Serial.println(servoAngle, DEC);
+            Serial.println("----");
+          } else {
+            Serial.println("Bad Input");
+          }
   }
   switch(servoSelect) {
     case 1:
