c6078552910ef28be6e50dd68c675b8158715653
bobotran <ryanhungtran5@gmail.com>
2019-11-09 10:07:08 -0800

    cleaned up cmdctr downlink using struct constructors

--- cmdctr/cmdctr_serializer.cpp
+++ cmdctr/cmdctr_serializer.cpp
@@ -16,99 +16,81 @@ using namespace Calstar;
 
 class cmdctr_serializer : public msg_ser
 {
-    private:
-        const uint8_t NUM_SERVOS = 3;
-        const uint8_t NUM_THERMOCOUPLES = 3;
-        const uint8_t NUM_PRESSURE_TRANSDUCERS = 0;
+private:
+  const uint8_t NUM_SERVOS = 3;
+  const uint8_t NUM_THERMOCOUPLES = 3;
+  const uint8_t NUM_PRESSURE_TRANSDUCERS = 0;
 
-        uint8_t flatbuf_buffer[FLATBUF_BUF_SIZE];
-        unsigned int buffer_len;
-// stubbed
-    public:
-        struct downlink_data {
-          PropDownlinkType Type;
-          bool Igniting;
-          float LoadCell;
-          bool FlowSwitch;
-          uint8_t Servos[NUM_SERVOS];
-          uint16_t Thermocouples[NUM_THERMOCOUPLES];
-          uint16_t PressureTransducers[NUM_PRESSURE_TRANSDUCERS];
+  uint8_t *flatbuf_buffer;
+  unsigned int buffer_len;
+  // stubbed
+public:
+  cmdctr_serializer(bf_len = 0): buffer_len(bf_len) {
+    flatbuf_buffer = new uint8_t[FLATBUF_BUF_SIZE];
+  }
+  ~cmdctr_serializer(){
+    if(flatbuf_buffer != nullptr) { delete[] flatbuf_buffer; }
+  }
+  struct downlink_data {
+    bool Igniting;
+    float LoadCell;
+    bool FlowSwitch;
+    uint8_t *Servos;
+    uint16_t *Thermocouples;
+    uint16_t *PressureTransducers;
 
-          downlink_data(){
-              Servos = new uint8_t[NUM_SERVOS];
-              Thermocouples = new uint16_t[NUM_THERMOCOUPLES];
-              PressureTransducers = new uint16_t[NUM_PRESSURE_TRANSDUCERS];
-          }
-          ~downlink_data() {
-            if Servos != nullptr {
-              delete[] Servos;
-            }
-            if Thermocouples != nullptr {
-              delete[] Thermocouples;
-            }
-            if PressureTransducers != nullptr {
-              delete[] PressureTransducers;
-            }
-          }
-        } ;
+    downlink_data(bool igniting, float loadCell, bool flowSwitch, Vector<uint8_t> *servos, Vector<uint16_t> *thermocouples, Vector<uint16_t> *pressureTransducers):
+    Igniting(igniting), LoadCell(loadCell), FlowSwitch(flowSwitch) {
+      Servos = new uint8_t[servos->size()];
+      std::copy(servos.begin(), servos.end(), Servos);
+      //memcpy(data.Servos, msg->Servos()->Data(), msg->Servos()->Data()->size());
 
-        struct uplink_data {
-            PropUplinkType Type;
-            uint8_t Servos[NUM_SERVOS];
+      Thermocouples = new uint8_t[thermocouples->size()];
+      std::copy(thermocouples.begin(), thermocouples.end(), Thermocouples);
 
-            uplink_data() {
-              Servos = new uint8_t[NUM_SERVOS];
-            }
+      PressureTransducers = new uint8_t[pressureTransducers->size()];
+      std::copy(pressureTransducers.begin(), pressureTransducers.end(), PressureTransducers);
+    }
+    ~downlink_data() {
+      if Servos != nullptr { delete[] Servos; }
+      if Thermocouples != nullptr { delete[] Thermocouples; }
+      if PressureTransducers != nullptr { delete[] PressureTransducers; }
+    }
+  } ;
 
-            ~uplink_data() {
-              if Servos != nullptr {
-                delete[] Servos;
-              }
-            }
-        } ;
+  struct uplink_data {
+    PropUplinkType Type;
+    uint8_t Servos[NUM_SERVOS];
 
-    cmdctr_serializer(){
-        buffer_len = 0;
+    uplink_data() {
+      Servos = new uint8_t[NUM_SERVOS];
     }
 
-    void update_buf(char c){
-
-        if (buffer_len == FLATBUF_BUF_SIZE) {
-            // If at end of buffer, shift and add to end
-            memmove(buffer, buffer + 1, FLATBUF_BUF_SIZE - 1);
-            buffer[FLATBUF_BUF_SIZE - 1] = (uint8_t) c;
-        } else {
-            // Otherwise build up buffer
-            buffer[buffer_len++] = (uint8_t) c;
-        }
+    ~uplink_data() {
+      if Servos != nullptr {
+        delete[] Servos;
+      }
     }
+  } ;
 
-    /**
-    * Returns: a struct downlink_data if flatbuf_buffer contains a valid downlink message.
-    **/
-    const downlink_data *getPropDownlinkData(){
-        msg_data incoming_msg = getIncomingMsg(flatbuf_buffer, buffer_len);
-        uint8_t actual_len = get_verified_length<PropDownlinkMsg>(VerifyPropDownlinkMsgBuffer, GetPropDownlinkMsg, incoming_msg.Payload, incoming_msg.Payload_Size);
-        if actual_len == 0 { return nullptr; }
-        // Now that we've read a valid message, copy it into the output
-        // buffer, then remove it from the input buffer and move
-        // everything else down. Then reduce current buffer length by
-        // the length of the processed message Then clear the rest of
-        // the buffer so that we don't get false positives with the
-        // verifiers
-        uint8_t actual_msg_buf[actual_len];
-        memcpy(actual_msg_buf, incoming_msg.Payload, actual_len);
-
-        const PropDownlinkMsg *actual_msg = GetPropDownlinkMsg(actual_msg_buf);
-
-        struct downlink_data data;
-        data.Igniting = actual_msg->Igniting();
-        data.LoadCell = actual_msg->LoadCell();
-        data.FlowSwitch = actual_msg->FlowSwitch();
-        memcpy(data.Servos, actual_msg->Servos()->Data(), actual_msg->Servos()->Data()->size()); //possible room for more efficiency
-        memcpy(data.Thermocouples, actual_msg->Thermocouples()->Data(), actual_msg->Thermocouples()->Data()->size());
-        memcpy(data.PressureTransducers, actual_msg->PressureTransducers()->Data(), actual_msg->PressureTransducers()->Data()->size());
-
-        return data;
+  void update_buf(char c){
+    if (buffer_len == FLATBUF_BUF_SIZE) {
+      // If at end of buffer, shift and add to end
+      memmove(flatbuf_buffer, flatbuf_buffer + 1, FLATBUF_BUF_SIZE - 1);
+      flatbuf_buffer[FLATBUF_BUF_SIZE - 1] = (uint8_t) c;
+    } else {
+      // Otherwise build up buffer
+      flatbuf_buffer[buffer_len++] = (uint8_t) c;
     }
+  }
+
+  /**
+  * Returns: a struct downlink_data if flatbuf_buffer contains a valid downlink message.
+  **/
+  const downlink_data *getPropDownlinkData(){
+    msg_data incoming_msg = getIncomingMsg(flatbuf_buffer, buffer_len);
+    PropDownlinkMsg *msg = get_specific_msg<PropDownlinkMsg>(VerifyPropDownlinkMsgBuffer, GetPropDownlinkMsg, incoming_msg.Payload, incoming_msg.Payload_Size);
+    downlink_data data(msg->Igniting(), msg->LoadCell(), msg->FlowSwitch(), msg->Servos(), msg->Thermocouples(), msg->PressureTransducers());
+    return data;
+  }
 }
--- general/msg_ser.cpp
+++ general/msg_ser.cpp
@@ -11,79 +11,37 @@ using namespace Calstar;
 class msg_ser
 {
 public:
-    const static int FLATBUF_BUF_SIZE = 256;
-    struct msg_data {
-      uint8_t FrameID;
-      uint8_t AckReqd;
-      uint8_t Bytes;
-      uint8_t *Payload;
-      uint8_t Payload_Size;
-      ~msg_data() {
-        if Payload != nullptr {
-          delete[] Payload;
-        }
-      }
-    };
+  const static int FLATBUF_BUF_SIZE = 256;
+  struct msg_data {
+    uint8_t FrameID;
+    uint8_t AckReqd;
+    uint8_t Bytes;
+    uint8_t *Payload;
+    uint8_t Payload_Size;
+    msg_data(uint8_t fid, uint8_t ack, uint8_t bytes, Vector<uint8_t> *payload):
+    FrameID(fid), AckReqd(ack), Bytes(bytes) {
+      Payload_Size = payload->size();
+      Payload = new uint8_t[Payload_Size];
+      std::copy(payload.begin(), payload.end(), Payload);
+    }
+    ~msg_data() { if (Payload != nullptr) { delete[] Payload; } }
+  };
 
-/**
-* Args: buffer and expected message length in bytes. Received from board-specific deserializers
-* Returns: msg struct. Clears processed message from input buffer
-*/
+  /**
+  * Args: buffer and expected message length in bytes. Received from board-specific deserializers
+  * Returns: msg struct. Clears processed message from input buffer
+  */
   msg_data* getIncomingMsg(uint8_t input_buffer, unsigned int len) {
-      uint8_t actual_len = get_verified_length(VerifyMsgBuffer, GetMsg, input_buffer, len);
-      if actual_len == 0 { return nullptr; }
-      /*
-      Verifier verifier(input_buffer, len);
-      if (VerifyMsgBuffer(verifier)) {
-          const Msg *msg = GetMsg(input_buffer);
-          // The message knows how big it should be
-          const uint8_t expectedBytes = msg->Bytes();
+    Msg *msg = get_flatbuffer_msg<Msg>(VerifyMsgBuffer, GetMsg, input_buffer, len);
+    if (msg == nullptr) {return nullptr; }
+    actual_len = msg->Bytes()
+    memmove(input_buffer, input_buffer + actual_len, FLATBUF_BUF_SIZE - actual_len);
+    len -= actual_len;
+    // Clear the rest of the buffer
+    memset(input_buffer + len, 0, FLATBUF_BUF_SIZE - len);
 
-          uint8_t actual_len = len;
-          if (len < expectedBytes) {
-              // The verifier will say we have a valid message even if
-              // we're a few bytes short Just read more characters at
-              // this point by returning early
-              return nullptr;
-          } else if (len > expectedBytes) {
-              // Now we want to verify that the "smaller buffer" with
-              // length equal to the expected number of bytes is
-              // actually a message in its own right (just a double
-              // check basically)
-              Verifier smallerVerifier(input_buffer, expectedBytes);
-              if (VerifyMsgBuffer(smallerVerifier)) {
-                  // If it is a message, then make sure we use the
-                  // correct (smaller) length
-                  actual_len = expectedBytes;
-              } else {
-                  // If it isn't valid, then this buffer just has
-                  // some malformed messages... continue and let's
-                  // get them out of the buffer by reading more
-                  return nullptr;
-              }
-          }
-          */
-          // Now that we've read a valid message, copy it into the output
-          // buffer, then remove it from the input buffer and move
-          // everything else down. Then reduce current buffer length by
-          // the length of the processed message Then clear the rest of
-          // the buffer so that we don't get false positives with the
-          // verifiers
-          uint8_t actual_msg_buf[actual_len];
-          memcpy(actual_msg_buf, input_buffer, actual_len);
-          memmove(input_buffer, input_buffer + actual_len, FLATBUF_BUF_SIZE - actual_len);
-          len -= actual_len;
-          // Clear the rest of the buffer
-          memset(input_buffer + len, 0, FLATBUF_BUF_SIZE - len);
-
-          const Msg *actual_msg = GetMsg(actual_msg_buf);
-          struct msg_data data;
-          data.FrameID = actual_msg->FrameID();
-          data.AckReqd = actual_msg->AckReqd();
-          data.Bytes = actual_msg->Bytes();
-          data.Payload = new uint8_t[payload_len];
-          return data;
-      }
+    msg_data data(msg->FrameID(), msg->AckReqd(), msg->Bytes(), msg->Payload());
+    return data;
   }
 
   msg* getOutgoingMsg(uint8_t buffer) {
@@ -91,44 +49,49 @@ public:
   }
 
   /**
-  * Args: buffer containing message to be verified, length of message in bytes
+  * Args:
+  <typename T>: type of flatbuffer message being checked. Ex: PropUplinkMsg, PropDownlinkMsg, Msg
+  is_valid: Flatbuffer-generated verifier function for this specific message type. Of the form Verify*Buffer
+  get_flatbuffer_msg: Flatbuffer-generated function that gets Flatbuffer message from bytes array
+  input_buffer: pointer to buffer to be checked for a valid message
+  len: length of input_buffer to check for a message
   * Returns:
-  * Verifies message is valid.
-  * If buffer contains valid message, return the actual length of the message
-  * If not, return 0
+  *   Verifies message is valid.
+  *   If buffer contains valid message, return the valid Flatbuffer Message
+      else returns nullptr
   */
-template <typename T>
-uint8_t get_verified_length(bool (*is_valid)(Verifier), *T (*get_flatbuffer_msg)(uint8_t), uint8_t input_buffer, unsigned int len) {
-  Verifier verifier(input_buffer, len);
-  if (is_valid(verifier)) {
-      const T *msg = get_flatbuffer_msg(input_buffer);
+  template <typename T>
+  uint8_t get_flatbuffer_msg(bool (*is_valid)(Verifier), *T (*get_specific_msg)(*uint8_t), uint8_t *input_buffer, unsigned int len) {
+    Verifier verifier(input_buffer, len);
+    if (is_valid(verifier)) {
+      const T *msg = get_specific_msg(input_buffer);
       // The message knows how big it should be
       const uint8_t expectedBytes = msg->Bytes();
 
-      uint8_t actual_len = len;
       if (len < expectedBytes) {
-          // The verifier will say we have a valid message even if
-          // we're a few bytes short Just read more characters at
-          // this point by returning early
-          //return nullptr;
-          return 0; // is this equivalent?
+        // The verifier will say we have a valid message even if
+        // we're a few bytes short Just read more characters at
+        // this point by returning early
+        return nullptr;
       } else if (len > expectedBytes) {
-          // Now we want to verify that the "smaller buffer" with
-          // length equal to the expected number of bytes is
-          // actually a message in its own right (just a double
-          // check basically)
-          Verifier smallerVerifier(input_buffer, expectedBytes);
-          if (is_valid(smallerVerifier)) {
-              // If it is a message, then make sure we use the
-              // correct (smaller) length
-              //actual_len = expectedBytes;
-              return expectedBytes;
-          } else {
-              // If it isn't valid, then this buffer just has
-              // some malformed messages... continue and let's
-              // get them out of the buffer by reading more
-              return 0; // is this equivalent?
-          }
+        // Now we want to verify that the "smaller buffer" with
+        // length equal to the expected number of bytes is
+        // actually a message in its own right (just a double
+        // check basically)
+        uint8_t smaller_msg_buf[expectedBytes];
+        memcpy(smaller_msg_buf, input_buffer, expectedBytes);
+        Verifier smallerVerifier(smaller_msg_buf, expectedBytes);
+        if (is_valid(smallerVerifier)) {
+          // If it is a message, then make sure we use the
+          // correct (smaller) length
+          return get_specific_msg(smaller_msg_buf);
+        } else {
+          // If it isn't valid, then this buffer just has
+          // some malformed messages... continue and let's
+          // get them out of the buffer by reading more
+          return nullptr;
+        }
       }
+    }
   }
 }
